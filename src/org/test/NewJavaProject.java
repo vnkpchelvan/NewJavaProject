e823698159f75c84c223d8f42250346614557172
vnkpchelvan <porchelvan2010@gmail.com>
2024-07-31 16:07:20 +0530

    new payment code added

--- src/org/test/NewJavaProject.java
+++ src/org/test/NewJavaProject.java
@@ -1,10 +1,15 @@
 package org.test;
 
 public class NewJavaProject {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("This is a new java project");
 	}
 
+	private void payment() {
+
+		System.out.println("New payment module");
+
+	}
 }
